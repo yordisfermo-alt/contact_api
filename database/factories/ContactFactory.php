ec82fac77668d0126480d682dc4e9185394f6ea5
yordisfermo-alt <yordisfermo@gmail.com>
2026-07-02 12:03:06 -0500

    Reajustes en el modelo contact, creacion de factory y sedeer.

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'phone_number' => fake()->unique()->numerify('3#########'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,33 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Contact;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            ->count(10)
+
+            ->create()
+
+            ->each(function ($user) {
+
+                Contact::factory()
+
+                    ->count(5)
+
+                    ->create([
+
+                        'user_id' => $user->id
+
+                    ]);
+
+            });
+
     }
 }
