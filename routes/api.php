204aa30f87fe5c1a733a01ffcebee0db56d8544f
yordisfermo-alt <yordisfermo@gmail.com>
2026-07-03 08:52:10 -0500

    implementacion de autenticacion y proteccion de las rutasde contactos

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
+// Rutas de autenticación 'Publicas'
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+
+// Rutas protegidas 'Tiene que estar autenticado'
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('contacts', ContactController::class);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 Route::apiResource('contacts', ContactController::class);
 
 Route::get('/user', function (Request $request) {
